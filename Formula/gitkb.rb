17c88c49029019dd24f7ac8b0de9bde1075f583f
Matt Walters <github@iammattwalters.com>
2026-04-17 23:01:32 -0500

    Update gitkb to v0.1.49 (#48)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/gitkb.rb
+++ Formula/gitkb.rb
@@ -3,29 +3,29 @@
 
 class Gitkb < Formula
   desc "Git-native knowledge base with AI-powered code intelligence"
-  homepage "https://github.com/harmony-labs/gitkb-releases"
-  version "0.1.47"
+  homepage "https://github.com/gitkb/gitkb-releases"
+  version "0.1.49"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/harmony-labs/gitkb-releases/releases/download/v#{version}/gitkb-darwin-arm64.tar.gz"
-      sha256 "5f6ebfeb1a7bf8cc09fbd5ca6ddc8ea1b728265d1cb406ed9ec3b2dcab0990e9"
+      url "https://github.com/gitkb/gitkb-releases/releases/download/v#{version}/gitkb-darwin-arm64.tar.gz"
+      sha256 "28601b5296d340a25a9b6c345a38e91d2abbf1371b1444907ec25bc3b4af6d34"
     end
     on_intel do
-      url "https://github.com/harmony-labs/gitkb-releases/releases/download/v#{version}/gitkb-darwin-x64.tar.gz"
-      sha256 "5c7fcdab7f09de7f74529cce3c413146d1972f1539e4003dd8abaf74f8794b3a"
+      url "https://github.com/gitkb/gitkb-releases/releases/download/v#{version}/gitkb-darwin-x64.tar.gz"
+      sha256 "bd0c5852d43493ee1f44d4ca8673661b16bac95782d4ea35aec2fa7606634a46"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/harmony-labs/gitkb-releases/releases/download/v#{version}/gitkb-linux-arm64.tar.gz"
-      sha256 "31664d01a48b1e556b9d7fbff26a70815f9cf9245c06ec82dfde29c38920d190"
+      url "https://github.com/gitkb/gitkb-releases/releases/download/v#{version}/gitkb-linux-arm64.tar.gz"
+      sha256 "10e710dc332342bf0cda2b2e0d3bce7ba731ad171a4c12fd8cfa59748d7c0189"
     end
     on_intel do
-      url "https://github.com/harmony-labs/gitkb-releases/releases/download/v#{version}/gitkb-linux-x64.tar.gz"
-      sha256 "63d41b4d968aab490926490944254de08417bd91a635d654984af15e34bc96a5"
+      url "https://github.com/gitkb/gitkb-releases/releases/download/v#{version}/gitkb-linux-x64.tar.gz"
+      sha256 "a1d4ed390d71e765eea878ad3b9876438779bb55cebaedd9d93f610b71fdc2e0"
     end
   end
 
