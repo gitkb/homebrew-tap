feb14ef7b85cf4c86b54b2c298800bcbaa29479d
Matt Walters <github@iammattwalters.com>
2026-04-29 17:39:06 -0500

    Update gitkb to v0.1.54 (#56)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/gitkb.rb
+++ Formula/gitkb.rb
@@ -4,28 +4,28 @@
 class Gitkb < Formula
   desc "Git-native knowledge base with AI-powered code intelligence"
   homepage "https://github.com/gitkb/gitkb-releases"
-  version "0.1.53"
+  version "0.1.54"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/gitkb/gitkb-releases/releases/download/v#{version}/gitkb-darwin-arm64.tar.gz"
-      sha256 "aba1f8c8b7f1d537df6644386295bdfbd158307a23267bac52320588b54bfa4d"
+      sha256 "dd1cee315b57f2d1d67c1c0a03f0c12a32830ce50678ef7f81bc4bbb31007f5c"
     end
     on_intel do
       url "https://github.com/gitkb/gitkb-releases/releases/download/v#{version}/gitkb-darwin-x64.tar.gz"
-      sha256 "3d3f728e91a0a5c18c2c55f3698fc1100feb6e77a85b11b53b955972970269a3"
+      sha256 "c23b67c8c6f1466349afd26f6298cd710023a2b5ca67189efdbff2ef1f7e49a2"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/gitkb/gitkb-releases/releases/download/v#{version}/gitkb-linux-arm64.tar.gz"
-      sha256 "c2305483b9c5fe5c222cedac000c711914ca47c31f4087f70c7b9c9aadd0345b"
+      sha256 "913a1f7cdfb1dbf23d6e67910efd5f90e733ad2391e7fe60c9ba6d749e059d6d"
     end
     on_intel do
       url "https://github.com/gitkb/gitkb-releases/releases/download/v#{version}/gitkb-linux-x64.tar.gz"
-      sha256 "80866a6becffd5979aa89b30648939cb5fa7fda0ab60905a70554411acdca2a6"
+      sha256 "cd78ae37b423fd56340650c2501062999a4875e3dda1637c0d2aee0a94e7bd46"
     end
   end
 
